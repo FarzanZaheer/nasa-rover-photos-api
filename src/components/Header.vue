e7ac7acebbc6a746ce554f94788baf6f744820a2
Farzan Zaheer <farzan.zaheer@time-change.de>
2024-05-26 19:16:20 +0200

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header">
+    <img height="300" alt="Vue logo" src="../assets/logo.png" />
+    <h1 class="title">The Mars Files</h1>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  text-align: center;
+}
+</style>
