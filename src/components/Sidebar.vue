f62ceafa529c178086873823531d62b99c4fd416
Farzan Zaheer <farzan.zaheer@time-change.de>
2024-05-26 19:14:28 +0200

    add sidebar component to select rovers

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="sidebar">
+    <div class="form-container">
+      <label>Rover: </label>
+      <select :value="rover" @change="onRoverSelect">
+        <option disabled value="">Please select rover</option>
+        <option value="curiosity">Curiosity</option>
+        <option value="opportunity">Opportunity</option>
+        <option value="spirit">Spirit</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const rover = computed(() => store.getters["apiModule/currentRover"]);
+
+const onRoverSelect = (event) => {
+  store.dispatch("apiModule/setRover", event.target.value);
+};
+</script>
+
+<style scoped>
+.sidebar {
+  background-color: #f0cacf;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 0 0 200px;
+}
+</style>
