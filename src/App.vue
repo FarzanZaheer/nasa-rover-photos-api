0dd3bba26e61b311427491b1cb132429372086cf
Farzan Zaheer <farzan.zaheer@time-change.de>
2024-05-26 19:19:03 +0200

    refactor app component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="The Mars Files" />
+    <div class="images-page-container">
+      <Sidebar />
+      <Content />
+    </div>
   </div>
 </template>
 
 <script>
+import Content from "./components/Content.vue";
+import Sidebar from "./components/Sidebar.vue";
 
 export default {
   name: "App",
   components: {
-
+    Sidebar,
+    Content,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.images-page-container {
+  display: flex;
 }
 </style>
