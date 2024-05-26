96c18260944a2fc2e8c844b03e7fd09a7659373a
Farzan Zaheer <farzan.zaheer@time-change.de>
2024-05-26 19:17:11 +0200

    add container content component

---
+++ src/components/Content.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="content-container">
+    <Header />
+    <ImageList />
+  </div>
+</template>
+
+<script setup>
+import Header from "./Header.vue";
+import ImageList from "./ImageList.vue";
+</script>
+
+<style scoped>
+.content-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-left: 90px;
+}
+</style>
